2fc72a65e924bffcc628ffd3e53263cdbc57f3e9
MurtazaBaranwala-Rite <115791527+MurtazaBaranwala-Rite@users.noreply.github.com>
2023-04-26 16:48:47 +0530

    first commit

---
+++ src/App.js
@@ -0,0 +1,30 @@
+import './App.css';
+import About from './components/About';
+import CreateUser from './components/CreateUser';
+import Login from './components/Login';
+import Navbar from './components/Navbar';
+import {BrowserRouter, Route, Routes} from "react-router-dom";
+
+
+function App() {
+
+
+    return (
+        <>
+            <BrowserRouter>
+                <Navbar mode={'light'}/>
+                <Routes>
+                    <Route exact path="/"
+                        element={<Login/>}/>
+                    <Route exact path="/create-user"
+                        element={<CreateUser/>}/>
+                    <Route exact path="/about"
+                        element={<About/>}/>
+                </Routes>
+
+            </BrowserRouter>
+        </>
+    );
+}
+
+export default App;
