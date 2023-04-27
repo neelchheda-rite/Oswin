65d8d3d9093d8b8b335daa04fb779a0ef03b6d52
MurtazaBaranwala-Rite <115791527+MurtazaBaranwala-Rite@users.noreply.github.com>
2023-04-27 14:58:56 +0530

    neww

--- src/components/Login.js
+++ src/components/Login.js
@@ -146,7 +146,6 @@ export default function SignInSide() {
               
                 <Grid item xs>
                   <Link to="/" variant="body2">
-                    
                     Forgot password?
                   </Link>
                 </Grid>
