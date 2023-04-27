9a3cb2b7ffc1c0db6733d2b664b4b659e0cbe44d
Neel Chheda <neel.chheda@ritetechnologies.co.in>
2023-04-27 19:12:36 +0530

    Summary Projection

--- src/components/Summary.js
+++ src/components/Summary.js
@@ -1,21 +1,29 @@
 import React from 'react';
-import Dropdown from 'react-bootstrap/Dropdown';
+import {Link} from 'react-router-dom';
+
 
 export default function Summary() {
-  return (
-    <>
-      <div className='container mt-3' style={{backgroundColor:"aqua"}}>
-      <Dropdown>
-      <Dropdown.Toggle variant="success" id="dropdown-basic" style={{width:"5rem",justifyItems:"right"}}>
-       
-      </Dropdown.Toggle>
-      <Dropdown.Menu>
-        <Dropdown.Item href="#/action-1">Action</Dropdown.Item>
-        <Dropdown.Item href="#/action-2">Another action</Dropdown.Item>
-        <Dropdown.Item href="#/action-3">Something else</Dropdown.Item>
-      </Dropdown.Menu>
-    </Dropdown>
-    </div>
+        return (<>
+            <div className='container mt-3  '>
+                <div className='row'>
+                <div className="col col-lg-1 label" >
+                    Customer :
+                </div>
+                <div class="dropdown col-md-auto">
+                    <Link class="btn btn-outline-secondary dropdown-toggle" to="/" role="button" data-bs-toggle="dropdown" aria-expanded="false">
+                       { " "}
+                    </Link>
+
+                    <ul class="dropdown-menu">
+                        <li>
+                            <Link className="dropdown-item" to="/">Action</Link>
+                        </li>
+                    </ul>
+                </div>
+                </div>
+            </div>
+
+        
     </>
-  )
+    )
 }
